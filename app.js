a73bef434efb4bcd9114630e5950014af8ff6be3
Romain Beaussart <36296521+RomainBeaussart@users.noreply.github.com>
2019-12-03 10:25:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -61,5 +61,10 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 module.exports = app;
