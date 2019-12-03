45d4a38a2216131297b2fb6d633efd7f20016b2a
Romain Beaussart <romain.beaussart@efrei.net>
2019-12-03 10:47:53 +0100

    Update

--- app.js
+++ app.js
@@ -38,27 +38,27 @@ app.use(cors({
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
 
-    // Pass to next layer of middleware
-    next()
+  // Pass to next layer of middleware
+  next()
 })
 
 /// catch 404 and forwarding to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
-    err.status = 404
-    next(err)
+  err.status = 404
+  next(err)
 })
 
 /// error handlers
@@ -68,18 +68,18 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500)
-        res.render('error', {
+    res.render('error', {
       message: err.message,
       error: err
     })
-    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
   res.status(err.status || 500)
-    res.render('error', {
+  res.render('error', {
     message: err.message,
     error: {}
   })
